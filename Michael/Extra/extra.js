754ebc025fc7635769a7032d614e8b8d193b2c5f
Michael Zechner <michael-zechner@gmx.at>
2021-09-29 23:53:05 +0200

    extra draft

--- Michael/Extra/extra.js
+++ Michael/Extra/extra.js
@@ -1,39 +1,36 @@
 class Hotel {
-  hotelName;
-  numberOfNights;
-  address;
-  image;
-  constructor(name, nightNumber, location, src) {
-    this.hotelName = name;
-    this.numberOfNights = nightNumber;
-    this.address = location;
-    this.image = src;
+  constructor(hotelName, numberOfNights, address, image) {
+    this.hotelName = hotelName;
+    this.numberOfNights = numberOfNights;
+    this.address = address;
+    this.image = image;
+  }
+  showAvailability() {
+    console.log(this.hotelName);
+    // let u = parseInt(document.getElementsByClassName("night-number")[0].value);
+    // let k = this.numberOfNights;
+    // if (u <= k) {
+    //   let z2 = document.getElementsByClassName("hotel-reserve")[0];
+    //   z2.style.display = "initial";
+    // } else {
+    //   console.log("Sorry, no available nights for " + this.hotelName);
+    // }
   }
   cardFunction() {
-    document.body.innerHTML += `<div id=hotel-${this.hotelName} class="card" style="width: 18rem;">
+    document.body.innerHTML += `<div class="card" style="width: 18rem;">
   <img src="${this.image}" class="card-img-top" alt="Hotel Pic">
   <div class="card-body">
     <h5 class="card-title">${this.hotelName}</h5>
     <p class="card-text">${this.address}</p>
-    <input id=nights-${this.hotelName} type="number">
-    <button type="button" id=button-${this.hotelName}-check class="btn btn-primary">Submit</button>
-    <button type="button" id=reservation-${this.hotelName} class="btn btn-success">I will reserve</button>
+    <input class=night-number type="number">
+    <button type="button" class="btn btn-primary night-check">Submit</button>
+    <button type="button" class="btn btn-success hotel-reserve">I will reserve</button>
   </div>
 </div>`;
-    let z = document.getElementById(`reservation-${this.hotelName}`);
+    let z = document.getElementsByClassName("hotel-reserve")[0];
     z.style.display = "none";
-    let a = document.getElementById(`button-${this.hotelName}-check`);
+    let a = document.getElementsByClassName("night-check")[0];
     a.addEventListener("click", this.showAvailability);
-  }
-  showAvailability() {
-    let u = document.getElementById(`nights-${this.hotelName}`);
-    let z = parseInt(u.value);
-    if (z <= this.numberOfNights) {
-      let z2 = document.getElementById(`reservation-${this.hotelName}`);
-      z2.style.display = "initial";
-    } else {
-      alert(`Sorry, no available nights for ${this.hotelName}`);
-    }
   }
 }
 var sunset = new Hotel(
@@ -42,5 +39,5 @@ var sunset = new Hotel(
   "Sunset Drive 108, 1200 California",
   "https://images.pexels.com/photos/1375383/pexels-photo-1375383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 );
-console.log(sunset);
 sunset.cardFunction();
+console.log(typeof sunset);
